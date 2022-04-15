caee1e07cb3200634bd0b39c8b605197adcab5a4
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-15 18:15:59 +0400

    work with action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import Example1 from './components/Example1';
+const redux = require("redux")
+
+function increment() {
+  return {
+      type: "INCREMENT"
+  }
+}
+
+function decrement() {
+  return {
+      type: "DECREMENT"
+  }
+}
+
+function double() {
+  return {
+      type: "DOUBLE"
+  }
+}
+
+function halve() {
+  return {
+      type: "HALVE"
+  }
+}
+
+function reducer(state = 0, action) {
+  switch(action.type) {
+      case "INCREMENT":
+          return state + 1
+      case "DECREMENT":
+          return state - 1
+      case "DOUBLE":
+          return state * 2
+      case "HALVE":
+          return Math.round(state / 2)
+      default:
+          return state
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-     {/* <Example1 /> */}
+
     </div>
   );
 }
