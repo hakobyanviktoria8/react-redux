05641574adf1267ebf03588230f2e552beb12cf2
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-15 21:51:52 +0400

    store dispatch action default value

--- src/App.js
+++ src/App.js
@@ -1,61 +1,69 @@
 import './App.css';
 const redux = require("redux")
 
-function increment(amount) {
-  return {
-      type: "INCREMENT",
-      payload: amount
+// function increment(amount) {
+//   return {
+//       type: "INCREMENT",
+//       payload: amount
+//   }
+// }
+
+// function decrement(amount) {
+//   return {
+//       type: "DECREMENT",
+//       payload: amount
+//   }
+// }
+// function double() {
+//   return {
+//     type: "DOUBLE"
+//   }
+// }
+
+// function halve() {
+//   return {
+//     type: "HALVE"
+//   }
+// }
+
+function changeState(num = 1){
+  return{
+    type:"CHANGE_STATE",
+    payload:num
   }
 }
-
-function decrement(amount) {
-  return {
-      type: "DECREMENT",
-      payload: amount
-  }
-}
-
-function double() {
-  return {
-      type: "DOUBLE"
-  }
-}
-
-function halve() {
-  return {
-      type: "HALVE"
-  }
-}
-
 function reducer(state = {count:0}, action) {
-  switch(action.type) {
-      case "INCREMENT":
-          return {
-            count:state.count + action.payload
-          }
-      case "DECREMENT":
-          return {
-            count: state.count - action.payload
-          }
-      case "DOUBLE":
-        return {
-          count: state.count * 2
-        }
-      case "HALVE":
-          return {
-            count: Math.round(state / 2)
-          }
-      default:
-          return state
+  // switch(action.type) {
+  //     case "INCREMENT":
+  //         return {
+  //           count:state.count + action.payload
+  //         }
+  //     case "DECREMENT":
+  //         return {
+  //           count: state.count - action.payload
+  //         }
+  //     case "DOUBLE":
+  //       return {
+  //         count: state.count * 2
+  //       }
+  //     case "HALVE":
+  //         return {
+  //           count: Math.round(state / 2)
+  //         }
+  //     default:
+  //         return state
+  // }
+  if(action.type ==="CHANGE_STATE"){
+    return {count : state.count+action.payload}
   }
 }
 
 const store = redux.createStore(reducer)
-console.log(store);
+// console.log(store);
 // dispatch / getState / replaceReducer / subscribe
 
 store.subscribe(()=>{
-  console.log(111,store.getState()) 
+  console.log("store.subscribe",store.getState()) 
 })
 
 // if added paylod didn't working
@@ -63,12 +71,15 @@ store.subscribe(()=>{
 // store.dispatch({type:"INCREMENT"})
 // store.dispatch({type:"DOUBLE"})
 // or we can call function
-store.dispatch(increment(99))
-store.dispatch(decrement(5))
-store.dispatch({type:"CHKA"})
+// store.dispatch(increment(99))
+// store.dispatch(decrement(5))
+// store.dispatch({type:"CHKA"})
 
 
-
+store.dispatch(changeState(9))
+store.dispatch(changeState(-909))
+store.dispatch(changeState(9909))
+store.dispatch(changeState())  //give default value
 
 
 
