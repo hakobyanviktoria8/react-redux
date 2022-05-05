6f53074c125ce9a5af3d72d7ce17bd066df6e0cb
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-05-06 00:29:26 +0400

    work count.js file, make methods +,-,change state, create stor, and subscribe store getState method

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import './App.css';
 import store from './redux';
-import {changeCount} from "./redux/count"
-import {addFavoriteThing, removeFavoriteThing} from "./redux/favoriteThings"
-import {setYouTubeTitle, incrementViewCount,upvoteVideo,downvoteVideo} from './redux/youTubeVideo';
+import {changeCount, decrement, increment} from "./redux/count"
 
 // count function
-store.dispatch(changeCount(23))
-
-
-// favorite Things function
-store.dispatch(addFavoriteThing("BlaBla"))
-store.dispatch(removeFavoriteThing("BlaBla"))
-store.dispatch(addFavoriteThing("Door bells"))
-store.dispatch(addFavoriteThing("Sleigh bells"))
-store.dispatch(removeFavoriteThing("Door bells"))
-
-// youtube function
-store.dispatch(setYouTubeTitle("New Title"))
-store.dispatch(incrementViewCount())
-store.dispatch(setYouTubeTitle("Learning Redux is Fun!"))
-store.dispatch(incrementViewCount())
-store.dispatch(upvoteVideo())
-store.dispatch(incrementViewCount())
-store.dispatch(upvoteVideo())
-store.dispatch(incrementViewCount())
-store.dispatch(upvoteVideo())
-store.dispatch(downvoteVideo())
-
+// store.dispatch(changeCount(23))
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(decrement())
 
 function App() {
   return (
     <div className="App">
       <h1>Redux</h1> 
-      <p> store getState count is a {store.getState().count}</p>
-      {console.log("store.getStat ",store.getState())}
+      <div className='countWrapper'>
+        <button onClick={decrement}>-</button>
+        <p>{store.getState().count}</p>
+        <button onClick={increment}>+</button>
+      </div>
+      {/* {console.log("store.getStat ",store.getState())} */}
     </div>
   );
 }
--- src/redux/count.js
+++ src/redux/count.js
@@ -1,3 +1,5 @@
+const redux = require("redux")
+
 export function changeCount(amount = 1) {
     return {
         type: "CHANGE_COUNT",
@@ -5,11 +7,39 @@ export function changeCount(amount = 1) {
     }
 }
 
+export function increment() {
+    return {
+        type: "INCREMENT"
+    }
+}
+
+export function decrement() {
+    return {
+        type: "DECREMENT"
+    }
+}
+
 export default function countReducer(count = 0, action) {
     switch(action.type) {
         case "CHANGE_COUNT":
             return count + action.payload
+        case "INCREMENT":
+            return count + 1
+        case "DECREMENT":
+            return count - 1
         default:
             return count
     }
 }
+
+const store = redux.createStore(countReducer)
+console.log("store",store); //store has dispatch, getState, replaceReducer, subscribe
+
+store.subscribe(()=>{
+    console.log("store.getState is Count ",store.getState())
+})
+
+store.dispatch(increment()) //1
+store.dispatch(increment())  //2
+store.dispatch(decrement()) //1
+store.dispatch(changeCount(11)) //1+11
